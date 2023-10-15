24c4fc334077cc9bccaf98a7cebbe49cb71583b0
vadimKapitan <xxxcap13@gmail.com>
2023-10-15 12:03:46 +0300

    Start

---
+++ src/main/java/pro/sky/part2hmw6/Part2hmw6Application.java
@@ -0,0 +1,12 @@
+package pro.sky.part2hmw6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Part2hmw6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Part2hmw6Application.class, args);
+	}
+}
